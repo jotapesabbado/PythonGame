7f56cbf3300c0b044b0fa8c28ce689ce7114b9eb
luisfelipe CowBoyAligator <luisfeliperuespereira@gmail.com>
2020-03-27 03:07:32 -0300

    Exercícios do professor João Pedro

--- Exercícios/00-Tipos_Primitivos_Conversoes.py
+++ Exercícios/00-Tipos_Primitivos_Conversoes.py
@@ -9,6 +9,48 @@ print(type(my_int), type(my_float), type(my_complex))
 
 
 """
-    Exercício 2:
+    Exercício 2, 3 e 4:
 """
-my_int=
+my_str= "1"
+print(float(my_int))
+print(str(my_float))
+print(int(my_str))
+print(int(my_str))
+my_strxx= """EU
+ACABEI
+DE
+PEIDAR
+"""
+"""
+Exercício 5
+"""
+#Pois elas armazenam elementos q podem ser identificados com uma chave(comando)
+"""
+Exercício 6 e 7
+"""
+my_srt2= "eu sou um lixo"
+print(my_srt2[10:15])
+print(len(my_srt2)) 
+"""
+Exercício 8
+"""
+#O caracter: \ 
+"""
+Exercício 9 e 10
+"""
+my_srt3= "meu cuzin tá doendo"
+print(my_srt3[4:9], my_srt3[13:22])
+#com o comando "in"
+x ="cuzin" in my_srt3
+print(x)
+
+"""
+Exercício 12, 13, 14 e 15
+"""
+#são valores binários q recebem apenas True ou False
+##Valores vazios, ex: 0, "", etc...
+print(bool(my_int))
+ 
+ 
+###0, ou seja, False
+
