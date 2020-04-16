db96e7f2464207fe4e232e4210d70a180ee6e2ec
luisfelipe CowBoyAligator <luisfeliperuespereira@gmail.com>
2020-04-16 13:59:59 -0300

    finalização de exercícios

--- Exercícios/03-Estruturas_Condicionais_If_Else.py
+++ Exercícios/03-Estruturas_Condicionais_If_Else.py
@@ -7,7 +7,7 @@ x = 30
 y = 30
 z = 29
 if x == y and y != z or y > z:
- print("comi o cu de quem ta lendo")
+    print("comi o cu de quem ta lendo")
 """
 #ela dá erro
 if x == y:
@@ -23,7 +23,7 @@ elif x > y:
     print("x é maior que y")
 
 """
-Exercício 6, 7, 8, 9, 10
+Exercício 6, 7, 8, 9, 10, 11
 """
 #caso o if de errado, ele irá rodar o else
 
@@ -31,11 +31,15 @@ Exercício 6, 7, 8, 9, 10
 
 #é pra aumentar a qualidade do seu código, fazendo mais coisas em menos espaço
 
-
-
+x = True
+z = '' 
+z = "sou um bosta:" + str(x) if x else "TOMA GARAI NÃO SOU UM BOSTA:" + str(x)
 print(z)
 
-
+x = True
+z = '' 
+z = "sou um bosta:{0}" if x else "TOMA GARAI NÃO SOU UM BOSTA:{0}"
+print (z.format(x)) 
 
 """
 Exercício 12 e 14
@@ -45,3 +49,23 @@ x3 = 10
 if x3 > 10:
  pass
 #use a palavra a keyword "pass" na estutura de condição
+
+
+x4 = 3
+if x4 > 2:
+    "cu"
+else:
+    if x4 < 2:
+        print("cuzin")
+
+if x4 == 2:
+    print("2")
+elif x4 == 5:
+    print("5")
+
+#if e if não podem ser encadeados, já o if / elif podem ser encadeados (switch case)
+
+#O python tem um switch case diferenciado, conhecido como if / elif
+
+
+
